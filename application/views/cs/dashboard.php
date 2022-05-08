bb15535f4fbebcf4a765bb20da9802d721311311
frostbittencode <71408543+frostbittencode@users.noreply.github.com>
2022-05-08 19:47:02 +0700

    Last TEst

--- application/views/cs/dashboard.php
+++ application/views/cs/dashboard.php
@@ -1,26 +1,29 @@
                 <!-- Begin Page Content -->
-                <div class="container-fluid" style="height:245px; background-color: rgba(255,74,59,1);">
+                <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <div class="py-2 mb-0">
-                        <h3 class="m-0 font-weight text-light"><b>Dashboard</h3>
-                        <h6 class="m-0 font-weight text-light">Pelayanan Pemeriksaan Laboratorium Klinik</h6>
+                    <div>
+                        <br>
+                        <h2 class="m-0 font-weight text-danger"><b>Dashboard</h2>
+                        <h6 class="m-0 font-weight text-danger">Pelayanan Pemeriksaan Laboratorium Klinik</h6>
                     </div>
 
-                    <div class="card mb-2 px-2">
+                    <br>
+
+                    <div class="card mb-4 px-2">
 
                         <div class="card-body">
 
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold mb-1 text-danger"><h4><b>Selamat Datang!</b><h4></div>
+                                    <div class="text-xs font-weight-bold mb-1 text-danger"><h4>Selamat Datang!<h4></div>
                                     <div class="text-xs font-weight text-gray-800"><h6>Hello, 
                                         <span class="badge badge-danger"><?php echo $empName; ?>.</span>
                                          Anda login sebagai <span class="badge badge-danger"><?php echo $roleName; ?>.</span><h6></div>
                                 </div>
 
                                 <div class="col-auto">
-                                    <img class="img-fluid px-5 px-sm-4 mt-0 mb-0" src="<?php echo base_url() ?>assets/img/cs.jpg" width=270px height=280px>
+                                    <img class="img-fluid px-5 px-sm-4 mt-3 mb-4" src="<?php echo base_url() ?>assets/img/cs.jpg" width=270px height=280px>
                                 </div>
                             </div>
                             
@@ -31,14 +34,14 @@
                     <div class="row">
 
                         <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-2">
-                            <div class="card border-left-danger py-0">
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-danger h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
                                                 Jumlah Pasien</div>
-                                            <div class="h6 mb-0 font-weight-bold text-gray-800">
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
                                                 <?php echo $viewCountVisitors ?></div>
                                             </div>
                                         <div class="col-auto">
@@ -50,14 +53,14 @@
                         </div>
 
                         <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-2">
-                            <div class="card border-left-danger py-0">
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-danger h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
                                                 Jumlah Sampel</div>
-                                            <div class="h6 mb-0 font-weight-bold text-gray-800"><?php echo $viewCountSamples ?></div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $viewCountSamples ?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-vials fa-2x text-gray-300"></i>
@@ -68,8 +71,8 @@
                         </div>
 
                         <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-2">
-                            <div class="card border-left-danger py-0">
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-danger h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
@@ -77,7 +80,7 @@
                                             </div>
                                             <div class="row no-gutters align-items-center">
                                                 <div class="col-auto">
-                                                    <div class="h6 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $viewCountLhus ?></div>
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $viewCountLhus ?></div>
                                                 </div>
                                             </div>
                                         </div>
@@ -90,14 +93,14 @@
                         </div>
 
                         <!-- Pending Requests Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-2">
-                            <div class="card border-left-danger py-0">
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-danger h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
                                                 Jumlah LHU</div>
-                                            <div class="h6 mb-0 font-weight-bold text-gray-800"><?php echo $viewCountLhu ?></div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $viewCountLhu ?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
